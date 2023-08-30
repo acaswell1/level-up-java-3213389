5b8c6e0f6505951c6ff2250a250df5654b9896eb
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-30 21:43:16 +0000

    Water Bill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,21 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class App {
 
+    private static final int CCF = 748;
+
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        int ccfs_used = (int) Math.ceil(gallonsUsage / CCF);
+        int ccfs_to_charge_for = ccfs_used - 2;
+        double additional_charges = 0.0;
+        if (ccfs_to_charge_for >= 1) {
+            additional_charges = ccfs_to_charge_for * 3.90;
+        }
+
+        return 18.84 + additional_charges;
     }
 
     public static void main(String[] args) {
