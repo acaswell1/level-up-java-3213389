183ab6f85b6408dc99c11a94d43881c87cd3a7f3
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-30 19:52:15 +0000

    password

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
+        int passwordLength = password.length();
+        if (passwordLength > 6) {
+            boolean capital = false, lowercase = false, num = false;
+            for (int i = 0; i < passwordLength; i++) {
+                char c = password.charAt(i);
+                if ((int) c >= 'A' && (int) c <= 'Z') {
+                    capital = true;
+                }
+                if ((int) c >= 'a' && (int) c <= 'z') {
+                    lowercase = true;
+                }
+                if ((int) c >= '0' && (int) c <= '1') {
+                    num = true;
+                }
+            }
+            if (capital && lowercase && num) {
+                return true;
+            }
+        }
         return false;
     }
 
