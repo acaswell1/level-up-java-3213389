5c8fd3327cbfa7fb86e223bb13916f1b61285951
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-30 22:11:58 +0000

    Person

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person alec = new Person("Alec", "Caswell", 22);
+        Person sam = new Person("Sam", "Bunker", 23);
+
+
+        alec.introduction();
+        sam.introduction();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,53 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
+
+  private String first_name = "";
+  private String last_name = "";
+  private int age = 0;
+
+  public Person(String fname, String lname, int age) {
+    this.first_name = fname;
+    this.last_name = lname;
+    this.age  = age;
+  }
+
+  public String getFirstName() {
+    return this.first_name;
+  }
+
+  public String getLastName() {
+    return this.last_name;
+  }
   
+  public String getFullName() {
+    return getFirstName() + " " + getLastName();
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setFirstName(String fname) {
+    this.first_name = fname;
+  }
+
+  public void setLastName(String lname) {
+    this.last_name = lname;
+  }
+  
+  public void setFullName(String fname, String lname) {
+    setFirstName(fname);
+    setLastName(lname);
+  }
+
+  public void getAge(int age) {
+    this.age = age;
+  }
+
+  public void introduction() {
+    System.out.println("Hello my name is " + this.first_name + " " + this.last_name + ". I am " + this.age + " years old.");
+  }
+
+
 }
