e318153eefa8697ac6ca47f690ada19f3125a0dd
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-31 19:13:44 +0000

    Invest ya change

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,10 +1,23 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.List;
+import java.lang.Math;
 
 public class App {
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
+        int num_purchases = purchases.size();
+
+        if (num_purchases == 0) {
+            return 0.0;
+        } 
+
+        double changeInvested = 0.0;
+
+        for (double purchase : purchases) {
+            changeInvested += (Math.ceil(purchase) - purchase);
+        } 
+        
+        return changeInvested / num_purchases;
     }
 
     public static void main(String[] args) {
