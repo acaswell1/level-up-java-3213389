98026702959e666d570bbcdcb88e649fad9739a4
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-31 17:24:02 +0000

    Thank god, we busted the non-volunteers

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,31 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static List<String> findStudentsWithIncompleteVolunteerEvents(
-            List<String> students,
-            Map<String, List<String>> attendeesMapping) {
-        // TODO: implement function
-        return List.of();
+
+    public static List<String> findStudentsWithIncompleteVolunteerEvents(List<String> students, Map<String, List<String>> attendeesMapping) {
+        List<String> vols = new ArrayList<>();
+        List<String> incomple_students = new ArrayList<>();
+        
+        attendeesMapping.forEach((fundraiser, students_present) -> students_present.forEach((student) -> vols.add(student)));
+        
+        for (String student : students) {
+            int num_occs = 0;
+            for (String stu : vols) {
+                if (stu == student) {
+                    num_occs++;
+                }
+            }
+            if (num_occs < 2) {
+                incomple_students.add(student);
+            }
+        }
+        
+        return incomple_students;
     }
 
     public static void main(String[] args) {
