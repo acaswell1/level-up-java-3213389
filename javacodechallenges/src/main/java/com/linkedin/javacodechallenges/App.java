f2fa6116f6fb0997c27fe92195bb625fd90fe8ad
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-31 23:30:09 +0000

    Redaction

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,15 +1,36 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.List;
 import java.util.Scanner;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class App {
     public static void redactTextFile(String fileName, String[] redactedWordsArray) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String lineHolder = "";
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("javacodechallenges/src/main/java/com/linkedin/javacodechallenges/" + fileName))) {
+            while ((lineHolder = reader.readLine()) != null) {
+                for (String redact : redactedWordsArray) {
+                    if (lineHolder.contains(redact)) {
+                        lineHolder = lineHolder.replaceAll(redact, "REDACTED");
+                    }
+                }
+                stringBuilder.append(lineHolder + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("./MacBeth.txt"))) {
+        String redacted_text = stringBuilder.toString();
 
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("javacodechallenges/src/main/java/com/linkedin/javacodechallenges/REDACTED_"
+                        + fileName))) {
+            writer.write(redacted_text);
         } catch (Exception e) {
             e.printStackTrace();
         }
