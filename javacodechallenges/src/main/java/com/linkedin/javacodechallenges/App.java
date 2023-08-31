af1c2bcbce6b81cb3cd9f8242128033eff8fc5ad
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-08-31 22:32:43 +0000

    DAD JOKES

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,39 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.*;
+import java.net.*;
+import org.json.*;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        // TODO: Call https://icanhazdadjoke.com/ API and display joke
+
+        StringBuilder dj_stringbldr = new StringBuilder();
+        String dad_joke= "";
+
+        try {
+            URL dj_url = new URL("https://icanhazdadjoke.com");
+            HttpURLConnection dj_conn = (HttpURLConnection) dj_url.openConnection();
+            dj_conn.setRequestProperty("Accept", "application/json");
+            dj_conn.setRequestMethod("GET");
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(dj_conn.getInputStream()))) {
+                String response = "";
+                while ((response = reader.readLine()) != null) {
+                    dj_stringbldr.append(response);
+                }
+                JSONObject dj_json = new JSONObject(dj_stringbldr.toString());
+                dad_joke = dj_json.getString("joke");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            dj_conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(dad_joke);
     }
 }
